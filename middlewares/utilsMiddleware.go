02fd64c8a2fbf865ec314ddae608e3e7d91d069d
William Vegas <elkareem123@gmail.com>
2023-01-08 02:30:08 -0400

    started with jwt and middlewares

---
+++ middlewares/utilsMiddleware.go
@@ -0,0 +1,7 @@
+package middlewares
+
+import "github.com/golang-jwt/jwt/v4"
+
+func Kek() {
+	jwt.Parse()
+}
--- services/sql.services.go
+++ services/sql.services.go
@@ -14,22 +14,6 @@ type Result struct {
 func GroupByService() []*Result {
 	db := db.DbConnection()
 
-	// Query using raw SQL sintax and GORM orm to get all the users in the users table
-	// and group them by their age.
-	// rows, err := db.Raw("SELECT actor_id, first_name, last_name FROM actor").Rows()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// // Print the rows
-	// for rows.Next() {
-	// 	var actor_id int
-	// 	var first_name string
-	// 	var last_name string
-	// 	rows.Scan(&actor_id, &first_name, &last_name)
-	// 	fmt.Println(actor_id, first_name, last_name)
-	// }
-
 	var result []*Result
 	db.Raw("SELECT first_name, actor_id, last_name FROM actor").Scan(&result)
 
