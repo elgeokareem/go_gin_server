415a60295041857424edc804e62fe4d89d0fcfbd
William Vegas <elkareem123@gmail.com>
2023-01-22 23:40:12 -0400

    added support for .ENV files

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"goGinServer/db"
 	"goGinServer/routes"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	db.DbConnection()
 
 	routes.Routes()
